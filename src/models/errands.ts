6af19d2c8d24ba2eeb6494d1fb2b25b60f8ee3f2
Jonas Silva <jonas86@edu.unisinos.br>
2023-06-06 22:05:24 -0300

    adicionando enum na classe errands

--- src/models/errands.ts
+++ src/models/errands.ts
@@ -1,9 +1,18 @@
 import { v4 as createUuid2 } from "uuid";
 
+export enum TypeErrands {
+  Public = "P",
+  Archived = "A",
+}
+
 export class Errands {
   public id: string;
 
-  constructor(private _title: string, private _description: string) {
+  constructor(
+    private _title: string,
+    private _description: string,
+    private _type: TypeErrands
+  ) {
     this.id = createUuid2();
   }
 }

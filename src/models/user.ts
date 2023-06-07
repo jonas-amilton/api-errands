e78118f89da69dee13781a192e0fc4ddd09df22b
Jonas Silva <jonas86@edu.unisinos.br>
2023-06-06 22:18:27 -0300

    criando metodos get de classe user

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,8 +4,16 @@ import { Errands } from "./errands";
 export class User {
   public id: string;
   private _errands: Errands[];
-  constructor(private _name: string, private email: string) {
+  constructor(private _name: string, private _email: string) {
     this.id = createUuid2();
     this._errands = [];
+  }
+
+  public get name(): string {
+    return this._name;
+  }
+
+  public get email(): string {
+    return this._email;
   }
 }

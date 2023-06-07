1c9a09b54398081d60b52bf5e834cd7c40b2801e
Jonas Silva <jonas86@edu.unisinos.br>
2023-06-06 22:01:20 -0300

    importando class errands em class user

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,11 @@
 import { v4 as createUuid2 } from "uuid";
+import { Errands } from "./errands";
 
 export class User {
   public id: string;
-
+  private _errands: Errands[];
   constructor(private _name: string, private email: string) {
     this.id = createUuid2();
+    this._errands = [];
   }
 }

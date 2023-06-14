a530479f95a6f07e34a33609431bd29755b27414
Jonas Silva <jonas86@edu.unisinos.br>
2023-06-13 22:01:28 -0300

    criando as rotas

--- src/routes/errands.routes.ts
+++ src/routes/errands.routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
+import { ErrandsController } from "../controllers/errands.controller";
 
 export const errandsRoutes = () => {
   const app = Router({
     mergeParams: true,
   });
 
-  app.post("/", [errandCheck], new ErrandsController().create);
-  app.get("/:errandid", new ErrandsController().get);
-  app.get("/", new ErrandsController().list);
-  app.delete("/:errandid", new ErrandsController().delete);
-  app.put("/:errandid", new ErrandsController().update);
+  app.post("/", new ErrandsController().create);
+  // app.get("/:errandid", new ErrandsController().get);
+  // app.get("/", new ErrandsController().list);
+  // app.delete("/:errandid", new ErrandsController().delete);
+  // app.put("/:errandid", new ErrandsController().update);
   return app;
 };
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -4,6 +4,12 @@ import { errandsRoutes } from "../routes/errands.routes";
 export const userRoutes = () => {
   const app = Router();
 
+  app.post("/", [userCheck, cpfCheck], new UserController().create);
+  app.get("/:id", new UserController().get);
+  app.get("/", new UserController().list);
+  app.delete("/:id", new UserController().delete);
+  app.put("/:id", new UserController().update);
+
   app.use("/:userid/errands", errandsRoutes());
 
   return app;

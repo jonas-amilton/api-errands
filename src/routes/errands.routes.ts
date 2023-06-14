ab515c7a030b3ca4131d691fd0dc83f0bf6ebfdf
Jonas Silva <jonas86@edu.unisinos.br>
2023-06-14 20:39:38 -0300

    adicionando middleware a rota de errands

--- src/routes/errands.routes.ts
+++ src/routes/errands.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { ErrandsController } from "../controllers/errands.controller";
+import { errandCheck } from "../middlewares/errand.middleware";
 
 export const errandsRoutes = () => {
   const app = Router({
     mergeParams: true,
   });
 
-  app.post("/", new ErrandsController().create);
+  app.post("/", [errandCheck], new ErrandsController().create);
   app.get("/:errandid", new ErrandsController().get);
   app.get("/", new ErrandsController().list);
   app.delete("/:errandid", new ErrandsController().delete);
